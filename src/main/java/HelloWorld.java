8043c755656e8ee0238d08f071526c19fae2f4c5
swathi vemula <91794472+universalit7@users.noreply.github.com>
2021-10-19 17:46:00 +0530

    Demo for git rebase

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,5 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("this is my first github committed");
         System.out.println("This is commit from feature into master");
+        
+        System.out.println("this demo is for git rebase from intellig");
     }
 }
