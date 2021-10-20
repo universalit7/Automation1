58f89cae7b1536aa80eed84775018bfab8765c84
swathi vemula <91794472+universalit7@users.noreply.github.com>
2021-10-20 20:35:28 +0530

    brand is intellig

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
         System.out.println("This is commit from feature into master");
         
         System.out.println("this demo is for git rebase from intellig");
+        System.out.println("this brand is intellig");
     }
 }
