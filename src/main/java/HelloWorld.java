dc237aff3dceffa6887dc334ff5941785f35ee36
swathi vemula <91794472+universalit7@users.noreply.github.com>
2021-10-21 20:18:40 +0530

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("this is my first github committed");
+        System.out.println("this is my first github commit integration with jenkins");
         System.out.println("This is commit from feature into master");
         
         System.out.println("this demo is for git rebase from intellig");
